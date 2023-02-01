6b6b07d18a828155e6e5426b110a1147a0490300
spynetS <alfred@stensatter.se>
2023-02-01 16:05:14 +0100

    update

---
+++ Assets/Menu.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+	// anmiate camera to menu position and start panel
+	// 
+}
