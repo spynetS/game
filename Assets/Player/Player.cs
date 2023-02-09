d1e200a583296928b9f93519fa6c492320c7613c
spynetS <alfred@stensatter.se>
2023-02-09 10:26:36 +0100

    hehe

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -5,9 +5,22 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    	public float points = 0;
-        public Gun gun;
+	// we will have a gameobject with the shape of a gun with a gun script witch will be responseble
+	// for the shooting mecanincs for each gun
+	// then we have a ui button with the gun as a child then when the player
+	// shooses a gun the players selected gun will be gatherd from the button press (child)
+	
+    public float points = 0;
+    public Gun gun;
+	public List<Button> boughtGuns = new List<Button>();
+	
 	public Text pointsText;
+		
+		void Start(){
+			foreach(Button b in boughtGuns){
+				
+			}
+		}
 	
 	// Update is called once per frame
     	void Update()
